487828f110e89a290645edf82208d6fb955614e6
Vachounet <vachounet@live.fr>
2019-06-27 16:01:17 +0200

    nash: update omni makefile

--- omni_nash.mk
+++ omni_nash.mk
@@ -1,8 +1,16 @@
-# Inherit some common Lineage stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Get the prebuilt list of APNs
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# must be before including omni part
+TARGET_BOOTANIMATION_SIZE := 1080p
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Vendor blobs
 $(call inherit-product-if-exists, vendor/motorola/nash/nash-vendor.mk)
@@ -10,9 +18,8 @@ $(call inherit-product-if-exists, vendor/motorola/nash/nash-vendor.mk)
 # Device
 $(call inherit-product, device/motorola/nash/device.mk)
 
-# Boot Animtion
-TARGET_BOOTANIMATION_HALF_RES := true
-
+# CarrierConfig
+PRODUCT_PACKAGE_OVERLAYS += vendor/omni/overlay/CarrierConfig
 
 # Device identifiers
 BUILD_FINGERPRINT := motorola/nash/nash:8.0.0/OPXS27.109-34-10/5:user/release-keys
